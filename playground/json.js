d0a5a7f7499e619d0b45503794a6b5e32efae688
joaovictor1205 <joaovictor1205>
2019-10-13 14:01:05 +0000

    chapter 4

---
+++ playground/json.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const person = {
+    name:'Andrew',
+    planet: 'Earth',
+    age: 27
+}
+
+const personJSON = JSON.stringify(person);
+
+fs.writeFileSync('personJSON.json', personJSON);
+
+const personBUFFER = fs.readFileSync('personJSON.json');
+const personRead = personBUFFER.toString();
+const personOBJ = JSON.parse(personRead);
+
+const newName = 'John';
+const newAge = 28;
+
+personOBJ.name = newName;
+personOBJ.age = newAge;
+
+const personReadJSON = JSON.stringify(personOBJ);
+
+fs.writeFileSync('personJSON.json', personReadJSON);
+
+
+/*
+const bookJSON = JSON.stringify(book);
+console.log(bookJSON);
+fs.writeFileSync('dataJSON.json', bookJSON);
+const dataBuffer = fs.readFileSync('dataJSON.json');
+console.log(dataBuffer.toString());
+*/
