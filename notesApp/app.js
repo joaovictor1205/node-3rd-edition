06d824195b6cbc4e252e71e069c0ffbac7c235b1
João Victor da Costa Gonçalves <joaog@kyros.com.br>
2019-10-14 13:55:23 -0300

    debugger

--- notesApp/app.js
+++ notesApp/app.js
@@ -1,6 +1,7 @@
 //const fs = require('fs')
 const getNotes = require('./notes');
 const validator = require('validator');
+const chalk = require('chalk');
 
 const message = getNotes();
 console.log(message);
@@ -9,11 +10,16 @@ console.log(message);
 //console.log(validator.isEmail('test@test.com'));
 
 const validation = validator.isURL('google.com');
-if(validation === false){
-    console.log('Is False');
-}else{
-    console.log('Is True');
 
+
+if(validation === false){
+    debugger;
+
+    console.log(chalk.red('Is False'));
+}else{
+    debugger;
+
+    console.log(chalk.green('Is True'));
 }
 
 //console.log(validator.isURL('google.com'));
