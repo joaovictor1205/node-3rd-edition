68a1798fc47e66366d38ed48297f8c7752cddc9d
João Victor da Costa Gonçalves <joaog@kyros.com.br>
2019-10-17 16:05:00 -0300

    async

--- asynchronous/app.js
+++ asynchronous/app.js
@@ -1,13 +1,8 @@
-console.log('Starting');
+const request = require('request')
 
+const url = 'https://api.darksky.net/forecast/1192755c8b22bc41c6b4ad153fe63254/37.8267,-122.4233'
 
-setTimeout(() => {
-    console.log('2 seconds Timer')
-}, 2000);
+request( { url: url, json:true }, (error, response) => {
+    console.log("It is currently" + response.body.currently.temperature + "degress out. There is a " + response.body.currently.precipProbability + "% chance of rain.");
+});
 
-setTimeout(() => {
-    console.log('0 seconds Timer')
-}, 0);
-
-
-console.log('Stopping');
